9b896b74619a01fb8c8248fbf4c6cca9738fb29c
Denis Bueno <dbueno@gmail.com>
2008-05-03 11:55:17 -0400

    license notice

--- Utils.hs
+++ Utils.hs
@@ -4,6 +4,26 @@
             ,FlexibleInstances
             ,FlexibleContexts #-}
 
+{-
+    This file is part of DPLLSat.
+
+    DPLLSat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DPLLSat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with DPLLSat.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2008 Denis Bueno
+-}
+
+
 {-|
 
 Generic utilities that happen to be used in the SAT solver.  In pretty much

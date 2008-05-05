e94b77e3a0853060f0fe758c1015c69ce65c8bd9
Denis Bueno <dbueno@gmail.com>
2008-05-05 15:13:31 -0400

    initial import

---
+++ Text/Tabular.hs
@@ -0,0 +1,53 @@
+
+{-|
+
+Tabular output.
+
+Converting any matrix of showable data types into a tabular form for which the
+layout is automatically done properly.  Currently there is no maximum row
+width, just a dynamically-calculated column width.
+
+If the input matrix is mal-formed, the largest well-formed submatrix is
+chosen.  That is, elements along too-long dimensions are chopped off.
+
+-}
+module Text.Tabular( T(..), mkTable ) where
+
+import Data.List( tails, intercalate )
+
+newtype T a = T [Row a]            -- table is a list of rows
+newtype Row a = Row [Cell a]
+data Cell a = Cell { cellWidth :: Int
+                   -- the width of a cell is the max of the widths of the
+                   -- string representations of all the elements in the column
+                   -- in which this cell occurs
+                   , cellData :: a } -- element printed in box of colWidth
+
+mkTable :: (Show a) => [[a]] -> T a
+mkTable rows = T $ mkRows rows
+  where
+    widths      = colWidths rows
+    mkRows rows = [ Row (map mkCell (zip widths row)) | row <- rows ]
+    mkCell      = uncurry Cell
+
+-- returns a list of the widths of each column
+colWidths :: (Show a) => [[a]] -> [Int]
+colWidths = map (maximum . map (length . show)) . zipn
+
+-- Pretty, columnar output.
+instance (Show a) => Show (T a) where
+    show (T rows) = intercalate "\n" $ map showRow rows 
+        where
+          showRow (Row cols) = intercalate " " $ colStrings
+            where
+              colStrings = [ padString (cellWidth c) (show d)
+                             | c@(Cell {cellData=d}) <- cols ]
+              maxColWidth = maximum (0:map length colStrings)
+
+padString maxWidth str = str ++ replicate padLen ' '
+    where padLen = maxWidth - length str
+
+zipn xss | any null xss = []
+zipn xss = map head xss : zipn (map tail xss)
+
+                  

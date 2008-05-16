1e9b04495495639fa76d598459d6bc6e15e277d5
Denis Bueno <dbueno@gmail.com>
2008-05-16 19:51:09 -0400

    renamed to the ssterrmonad

--- DPLL/Monad.hs
+++ DPLL/Monad.hs
@@ -4,9 +4,12 @@
             ,FlexibleInstances
  #-}
 
-{-| The main SAT solver monad.  Embeds `ST'.  See type `DPLLMonad'.
+{-|
 
-Most of the work done is in the form of `DPLLMonad' actions. -}
+The main SAT solver monad.  Embeds `ST'.  See type `SSTErrMonad', which stands
+for ''State ST Error Monad''.
+
+Most of the work done is in the form of `SSTErrMonad' actions. -}
 module DPLL.Monad where
 
 import Control.Monad.Error hiding ((>=>), forM_)
@@ -15,47 +18,47 @@ import Control.Monad.State.Lazy hiding ((>=>), forM_)
 import Control.Monad.MonadST
 
 
-instance MonadST s (DPLLMonad e st s) where
+instance MonadST s (SSTErrMonad e st s) where
     liftST = dpllST
 
 -- | Perform an @ST@ action in the DPLL monad.
-dpllST :: ST s a -> DPLLMonad e st s a
+dpllST :: ST s a -> SSTErrMonad e st s a
 {-# INLINE dpllST #-}
-dpllST st = DPLLMonad (\k s -> st >>= \x -> k x s)
+dpllST st = SSTErrMonad (\k s -> st >>= \x -> k x s)
 
--- | @runDPLLMonad m s@ executes a `DPLLMonad' action with initial state @s@
+-- | @runSSTErrMonad m s@ executes a `SSTErrMonad' action with initial state @s@
 -- until an error occurs or a result is returned.
-runDPLLMonad :: (Error e) => DPLLMonad e st s a -> (st -> ST s (Either e a, st))
-runDPLLMonad m = unDPLLMonad m (\x s -> return (return x, s))
+runSSTErrMonad :: (Error e) => SSTErrMonad e st s a -> (st -> ST s (Either e a, st))
+runSSTErrMonad m = unSSTErrMonad m (\x s -> return (return x, s))
 
-execDPLLMonad :: (Error e) => DPLLMonad e st s a -> st -> ST s (Either e a)
-execDPLLMonad m s = do (result, _) <- runDPLLMonad m s
-                       return result
+execSSTErrMonad :: (Error e) => SSTErrMonad e st s a -> st -> ST s (Either e a)
+execSSTErrMonad m s = do (result, _) <- runSSTErrMonad m s
+                         return result
 
--- | @DPLLMonad e st s a@: the error type @e@, state type @st@, @ST@ thread
+-- | @SSTErrMonad e st s a@: the error type @e@, state type @st@, @ST@ thread
 -- @s@ and result type @a@.
 --
 -- This is a monad embedding @ST@ and supporting error handling and state
 -- threading.  It uses CPS to avoid checking `Left' and `Right' for every
 -- `>>='; instead only checks on `catchError'. Idea adapted from
 -- <http://haskell.org/haskellwiki/Performance/Monads>.
-newtype DPLLMonad e st s a =
-    DPLLMonad { unDPLLMonad :: forall r. (a -> (st -> ST s (Either e r, st)))
+newtype SSTErrMonad e st s a =
+    SSTErrMonad { unSSTErrMonad :: forall r. (a -> (st -> ST s (Either e r, st)))
                               -> (st -> ST s (Either e r, st)) }
 
-instance Monad (DPLLMonad e st s) where
-    return x = DPLLMonad ($ x)
-    m >>= f  = DPLLMonad (\k -> unDPLLMonad m (\a -> unDPLLMonad (f a) k))
+instance Monad (SSTErrMonad e st s) where
+    return x = SSTErrMonad ($ x)
+    m >>= f  = SSTErrMonad (\k -> unSSTErrMonad m (\a -> unSSTErrMonad (f a) k))
 
-instance MonadState st (DPLLMonad e st s) where
-    get = DPLLMonad (\k s -> k s s)
-    put s' = DPLLMonad (\k _ -> k () s')
+instance MonadState st (SSTErrMonad e st s) where
+    get = SSTErrMonad (\k s -> k s s)
+    put s' = SSTErrMonad (\k _ -> k () s')
 
-instance (Error e) => MonadError e (DPLLMonad e st s) where
+instance (Error e) => MonadError e (SSTErrMonad e st s) where
     throwError err =            -- throw away continuation
-        DPLLMonad (\_ s -> return (Left err, s))
-    catchError action handler = DPLLMonad
-        (\k s -> do (x, s') <- runDPLLMonad action s
+        SSTErrMonad (\_ s -> return (Left err, s))
+    catchError action handler = SSTErrMonad
+        (\k s -> do (x, s') <- runSSTErrMonad action s
                     case x of
-                      Left error -> unDPLLMonad (handler error) k s'
+                      Left error -> unSSTErrMonad (handler error) k s'
                       Right result -> k result s')

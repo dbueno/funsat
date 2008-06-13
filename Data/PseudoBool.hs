4cc8183cac9d63c0a1a3cd50082e51e23646fad5
Denis Bueno <dbueno@gmail.com>
2008-06-13 18:06:36 -0400

    Module comment about direction.

--- Data/PseudoBool.hs
+++ Data/PseudoBool.hs
@@ -1,3 +1,10 @@
+
+-- | This module is intended for adding the ability to translate (general?)
+-- pseudo-boolean constraints for Funsat.
+--
+--  * There are polynomial-size BDDs for cardinality constraints, that is,
+--  constraints of the form /x1 + ... + xn >= k/.  That should probably be the
+--  starting point.
 module Data.PseudoBool where
 
 -- for a package:

ebda133573e8e13b1501ebd55b2542d75ecbccc3
Denis Bueno <dbueno@gmail.com>
2008-07-17 15:49:10 -0400

    Try to deepSeq stuff to get an idea of how long it takes to parse, but, couldn't.

--- parse-dimacs/tests/Main.hs
+++ parse-dimacs/tests/Main.hs
@@ -1,7 +1,24 @@
 module Main where
 
-import Language.CNF.Parse.ParseDIMACS( parseFile )
+import Language.CNF.Parse.ParseDIMACS
 import System.Environment( getArgs )
+import Text.Parsec.Error( ParseError )
 
 main :: IO ()
 main = getArgs >>= parseFile . head >>= print
+
+class DeepSeq a where
+    deepSeq :: a -> b -> b
+
+instance DeepSeq Bool where deepSeq = seq
+instance DeepSeq Int where deepSeq = seq
+instance DeepSeq a => DeepSeq [a] where
+    deepSeq [] x = x
+    deepSeq (x:xs) y = deepSeq x $ deepSeq xs y
+instance  (DeepSeq a, DeepSeq b) => DeepSeq (Either a b)  where
+    deepSeq (Left  a) y = deepSeq a y
+    deepSeq (Right b) y = deepSeq b y
+instance DeepSeq CNF where
+    deepSeq c y = deepSeq (numVars c) . deepSeq (numClauses c) . deepSeq (clauses c)
+                  $ y
+instance DeepSeq ParseError where deepSeq = seq

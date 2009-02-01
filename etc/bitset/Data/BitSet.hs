505e06347cd346fdd6b39a7a81f0a7eb36030003
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-02-01 19:55:44 +0100

    Remove Hash class. Use the standard Enum class instead.

--- etc/bitset/Data/BitSet.hs
+++ etc/bitset/Data/BitSet.hs
@@ -2,13 +2,11 @@
 -- into (non-negative) @Int@s.  Supports insertion, deletion, size, and
 -- membership testing, and is completely pure (functional).
 --
--- To use this library, simply supply a `Hash' instance for your data type.
--- (See the `Hash' class for the relevant laws your instance must obey.)  Each
--- operation requires at most one call to `hash'.  It is important that the
--- values you intend to keep track of start from 0 and go up.  A value which
--- `hash'es to @n@ corresponds to bit location @n@ in an @Integer@, and thus
--- requires that @Integer@ to have at least @n@ bits.  Don't shoot yourself in
--- the foot by `hash'ing to big @Int@s.
+-- To use this library, simply supply a `Enum' instance for your data type or
+-- have it derived. It is important that the values you intend to keep track
+-- of start from 0 and go up. A value for which @fromEnum x@ is @n@
+-- corresponds to bit location @n@ in an @Integer@, and thus requires that
+-- @Integer@ to have at least @n@ bits.
 --
 -- The implementation is quite simple: we rely on the @Bits Integer@ instance
 -- from @Data.Bits@.  An advantage of this library over simply using that
@@ -16,8 +14,7 @@
 -- The interface we expose ensures client code will not typecheck if it
 -- confuses two bit sets intended to keep track of different types.
 module Data.BitSet
-    ( Hash(..)
-    , BitSet
+    ( BitSet
     , empty
     , null
     , insert
@@ -29,24 +26,6 @@ module Data.BitSet
 import Prelude hiding ( null )
 import Data.Bits
 
-
--- | Map a value to an non-negative @Int@.
---
--- For the implementation to give reliable results, it must be that if @hash x
--- == hash y@, @x@ and @y@ are equivalent under the relevant relation used in
--- the client code.  (We don't depend on equality, but the client code will
--- certainly want to use the above sort of inference.)
---
--- In fact, if the relevant relation is @==@, the following quickcheck
--- test should pass, for arbitrary @x@ and @y@ of type @a@:
---
--- @prop_hash x y =
---     if hash x == hash y then x == y
---     else x /= y
---     && if x == y then hash x == hash y
---        else hash x /= hash y@
-class Hash a where
-    hash :: a -> Int
 
 -- | The @Show@ instance kind of sucks.  It just shows the size paired with
 -- the internal @Integer@ representation.  A good show would probably show all
@@ -64,13 +43,13 @@ empty :: BitSet a
 null :: BitSet a -> Bool
 
 -- | /O(setBit on Integer)/ Insert an item into the bit set.
-insert :: Hash a => a -> BitSet a -> BitSet a
+insert :: Enum a => a -> BitSet a -> BitSet a
 
 -- | /O(clearBit on Integer)/ Delete an item from the bit set.
-delete :: Hash a => a -> BitSet a -> BitSet a
+delete :: Enum a => a -> BitSet a -> BitSet a
 
 -- | /O(testBit on Integer)/ Ask whether the item is in the bit set.
-member :: Hash a => a -> BitSet a -> Bool
+member :: Enum a => a -> BitSet a -> Bool
 
 -- | /O(1)/ The number of elements in the bit set.
 size :: BitSet a -> Int
@@ -83,34 +62,17 @@ empty = BS (0, 0)
 null (BS (n, _)) = n == 0
 
 {-# INLINE insert #-}
-insert x s@(BS (count, i)) = BS $ (count', setBit i h)
-    where count' = if h `memHash` s then count else count+1
-          h      = hash x
+insert x (BS (count, i)) = BS $ (count', setBit i e)
+    where count' = if testBit i e then count else count+1
+          e      = fromEnum x
 
 {-# INLINE delete #-}
-delete x s@(BS (count, i)) = BS $ (count', clearBit i h)
-    where count' = if h `memHash` s then count-1 else count
-          h      = hash x
+delete x (BS (count, i)) = BS $ (count', clearBit i e)
+    where count' = if testBit i e then count-1 else count
+          e      = fromEnum x
 
 {-# INLINE member #-}
-member x s = hash x `memHash` s
-memHash :: Int -> BitSet a -> Bool
-{-# INLINE memHash #-}
-memHash h (BS (_, i)) = testBit i h
+member x (BS (_, i)) = testBit i (fromEnum x)
 
 {-# INLINE size #-}
 size (BS (count, _)) = count
-
--- * Default instances
-
-instance Hash Int where
-    hash = id
-
-instance Hash Integer where
-    hash = fromIntegral
-
--- Needs UndecidableInstances?
--- instance Integral a => Hash a where
---     hash = fromIntegral
-
-    
